0dabdff9f5f22d911e49634069afc8ce2f4ef147
Harri Klingsten <powercommand2022@gmail.com>
2023-01-16 12:55:12 +0100

    First version of DiskCommand

---
+++ src/PainKiller.PowerCommands.KubernetesCommands/Commands/DiskCommand.cs
@@ -0,0 +1,26 @@
+﻿using PainKiller.PowerCommands.KubernetesCommands.DomainObjects;
+
+namespace PainKiller.PowerCommands.KubernetesCommands.Commands;
+
+[PowerCommandTest(tests:" ")]
+[PowerCommandDesign( description: "List PersistentVolumes sorted by capacity",
+    arguments: "",
+    example: "storage")]
+public class DiskCommand : CommandBase<PowerCommandsConfiguration>
+{
+    public DiskCommand(string identifier, PowerCommandsConfiguration configuration) : base(identifier, configuration) { }
+
+    public override RunResult Run()
+    {
+        var nSpaceName = Input.SingleArgument;
+        WriteCodeExample("kubectl",$"get pv --sort-by=.spec.capacity.storage");
+        ShellService.Service.Execute("kubectl",$"get pv --sort-by=.spec.capacity.storage","", ReadLine,"", waitForExit: true);
+        var response = LastReadLine;
+        var rows = response.Split('\n').Skip(1).ToList();
+        if (rows.Count <= 0) return Ok();
+
+        var details = rows.Select(r => new StorageDetailItem(r)).Where(s => !string.IsNullOrEmpty(s.Name)).ToList();
+        ConsoleTableService.RenderTable(details, this);
+        return Ok();
+    }
+}
---
+++ src/PainKiller.PowerCommands.KubernetesCommands/DomainObjects/StorageDetailItem.cs
@@ -0,0 +1,18 @@
+﻿namespace PainKiller.PowerCommands.KubernetesCommands.DomainObjects;
+public class StorageDetailItem
+{
+    public StorageDetailItem(){}
+    public StorageDetailItem(string row)
+    {
+        var cols = row.Split(' ').Where(r => !string.IsNullOrEmpty(r)).ToArray();
+        if(cols.Length == 0) return;
+        Name = cols[0];
+        Capacity = cols[1];
+        Status = cols[4];
+        Claim = cols[5];
+    }
+    public string Name { get; set; }
+    public string Capacity { get; set; }
+    public string Claim { get; set; }
+    public string Status { get; set; }
+}
