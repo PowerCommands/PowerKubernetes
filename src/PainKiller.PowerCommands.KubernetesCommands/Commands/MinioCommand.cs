9cd88da2e766bc1807fced778e9207a60127c003
Harri Klingsten <harri.klingsten2@scb.se>
2023-03-08 19:04:14 +0100

    .

--- src/PainKiller.PowerCommands.KubernetesCommands/Commands/MinioCommand.cs
+++ src/PainKiller.PowerCommands.KubernetesCommands/Commands/MinioCommand.cs
@@ -17,8 +17,7 @@ public class MinioCommand : CommandBase<PowerCommandsConfiguration>
         {
             var path = Path.Combine(ConfigurationGlobals.ApplicationDataFolder, "Docker", "data");
             if (!Directory.Exists(path)) Directory.CreateDirectory(path);
-            //docker run -p 9000:9000 -p 9090:9090 --name minio1 -v D:\minio\data:/data -e "MINIO_ROOT_USER=admin" -e "MINIO_ROOT_PASSWORD=abc123" quay.io/minio/minio:latest server /data --console-address ":9090"
-            ShellService.Service.Execute("docker",$"run -p 9000:9000 -p 9090:9090 --name {containerName} -v {path}:/data -e \"MINIO_ROOT_USER=minioadmin\" -e \"MINIO_ROOT_PASSWORD=minioadmin\" quay.io/minio/minio:latest server /data --console-address \":9090",CdCommand.WorkingDirectory, WriteLine,"", waitForExit: true);
+            ShellService.Service.Execute("docker",$"run -p 9000:9000 -p 9090:9090 --name {containerName} -v {path}:/data -e \"MINIO_ROOT_USER=minioadmin\" -e \"MINIO_ROOT_PASSWORD=minioadmin\" quay.io/minio/minio:latest server /data --console-address \":9090",CdCommand.WorkingDirectory, WriteLine,"", waitForExit: false);
             WriteSuccessLine("MinIO container spinning up...");
             Thread.Sleep(5000);
             ShellService.Service.OpenWithDefaultProgram("http://localhost:9000");
@@ -28,9 +27,9 @@ public class MinioCommand : CommandBase<PowerCommandsConfiguration>
 
     private void Remove(string containerName)
     {
-        ShellService.Service.Execute("docker","stop minio1",CdCommand.WorkingDirectory, WriteLine,"", waitForExit: true);
+        ShellService.Service.Execute("docker","stop minio1",CdCommand.WorkingDirectory, WriteLine,"", waitForExit: false);
         WriteSuccessLine("MinIO container minio1 stopped.");
-        ShellService.Service.Execute("docker","rm minio1",CdCommand.WorkingDirectory, WriteLine,"", waitForExit: true);
+        ShellService.Service.Execute("docker","rm minio1",CdCommand.WorkingDirectory, WriteLine,"", waitForExit: false);
         WriteSuccessLine("MinIO container minio1 removed.");
 
     }
