17a745f32efcd68a5ea1c5ccf0fbfe1a1078a428
Harri Klingsten <harri.klingsten2@scb.se>
2023-05-28 00:57:07 +0200

    Inspect command working.

--- src/PainKiller.PowerCommands.KubernetesCommands/Commands/InspectCommand.cs
+++ src/PainKiller.PowerCommands.KubernetesCommands/Commands/InspectCommand.cs
@@ -4,7 +4,8 @@ using PainKiller.PowerCommands.KubernetesCommands.DomainObjects;
 namespace PainKiller.PowerCommands.KubernetesCommands.Commands;
 
 [PowerCommandDesign( description: "Inspect the digest and signature of  your docker image",
-                         options: ".!key",
+                       arguments: "!image-name",
+                         options: "!KEY|show-raw-data",
                          example: "inspect <image-name> --key <key-name>")]
 public class InspectCommand : CommandBase<PowerCommandsConfiguration>
 {
@@ -14,21 +15,38 @@ public class InspectCommand : CommandBase<PowerCommandsConfiguration>
     {
         var imageName = Input.SingleArgument;
         var keyName = GetOptionValue("key");
-        Inspect(imageName, keyName);
+        var showRawData = HasOption("show-raw-data");
+        Inspect(imageName, keyName, showRawData);
         return Ok();
     }
 
-    public void Inspect(string imageName, string keyName)
+    public void Inspect(string imageName, string keyName, bool showRawData)
     {
         WriteHeadLine("First step is to inspect the digest of the image. ");
         ShellService.Service.Execute("docker", $"manifest inspect --verbose {imageName}", AppContext.BaseDirectory, ReadLine, "", waitForExit: true);
         var manifest = JsonSerializer.Deserialize<ContainerManifest>(LastReadLine);
         var imageDigest = manifest?.Descriptor.digest;
         WriteCodeExample("Image digest     :", $"{imageDigest}");
+        if(showRawData) WriteLine(LastReadLine);
         
-        ShellService.Service.Execute("docker", $"trust inspect --pretty {imageName}", AppContext.BaseDirectory, ReadLine, "", waitForExit: true);
-        
-        ShellService.Service.Execute("docker", $"trust inspect --pretty --key <public-key> <image-name>", AppContext.BaseDirectory, ReadLine, "", waitForExit: true);
+        ShellService.Service.Execute("docker", $"trust key load {keyName}.pub", Path.Combine(ConfigurationGlobals.ApplicationDataFolder, "powerkubernetes\\docker"), ReadLine, "", waitForExit: true);
+        //ShellService.Service.Execute("docker", $"trust key load {keyName}.key", Path.Combine(ConfigurationGlobals.ApplicationDataFolder, "powerkubernetes\\docker"), ReadLine, "", waitForExit: true);
         WriteLine(LastReadLine);
+        ShellService.Service.Execute("docker", $"trust inspect {imageName}", AppContext.BaseDirectory, ReadLine, "", waitForExit: true);
+        var jsonData = $"{{ \"Signatures\": {LastReadLine}}}";
+        var imageKeysMetadata = JsonSerializer.Deserialize<ImageKeysMetadata>(jsonData) ?? new ImageKeysMetadata();
+        foreach (var signature in imageKeysMetadata.Signatures)
+        {
+            WriteLine($"Name: {signature.Name}");
+            WriteHeadLine("Signed tags:");
+            foreach (var signedTag in signature.SignedTags)
+            {
+                WriteLine($"Signed tag: {signedTag.SignedTag}");
+                WriteLine($"Digest: {signedTag.Digest}");
+            }
+            WriteHeadLine("Signers:");
+            foreach (var signer in signature.Signers) WriteLine($"Signed tag: {signer.Name}");
+        }
+        if(showRawData) WriteLine(LastReadLine);
     }
 }
---
+++ src/PainKiller.PowerCommands.KubernetesCommands/DomainObjects/ImageKeysMetadata.cs
@@ -0,0 +1,42 @@
+﻿namespace PainKiller.PowerCommands.KubernetesCommands.DomainObjects;
+
+public class ImageKeysMetadata
+{
+    public Signatures[] Signatures { get; set; }
+}
+public class Signatures
+{
+    public string Name { get; set; }
+    public Signedtag[] SignedTags { get; set; }
+    public Signer[] Signers { get; set; }
+    public Administrativekey[] AdministrativeKeys { get; set; }
+}
+
+public class Signedtag
+{
+    public string SignedTag { get; set; }
+    public string Digest { get; set; }
+    public string[] Signers { get; set; }
+}
+
+public class Signer
+{
+    public string Name { get; set; }
+    public Key[] Keys { get; set; }
+}
+
+public class Key
+{
+    public string ID { get; set; }
+}
+
+public class Administrativekey
+{
+    public string Name { get; set; }
+    public Key1[] Keys { get; set; }
+}
+
+public class Key1
+{
+    public string ID { get; set; }
+}
